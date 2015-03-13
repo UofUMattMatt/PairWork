1a3f0a2793726b9c677d046fee5cbb1696717f9b
mahowa <jdbball1@gmail.com>
2015-03-13 12:30:58 -0600

    Created Classes

---
+++ GitHubBrowser/ClassLibrary1/languages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class languages
+    {
+        public languages(string t, int l)
+        {
+            type = t;
+            lines = l;
+        }
+        public string type { get; private set; }
+        public int lines { get; private set; }
+    }
+}
---
+++ GitHubBrowser/ClassLibrary1/repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    public class Repository
+    {
+        public Repository(string username, string repname, string description, HashSet<languages> languages, string avatar, string email)
+        {
+            Username = username;
+            RepName = repname;
+            Description = description;
+            Languages = languages;
+            Avatar = avatar;
+            Email = email;
+        }
+      public string Username {get;private set;}
+      public string RepName { get; private set; }
+      public string Description { get; private set; }
+      public HashSet<languages> Languages { get; private set; }
+      public string Avatar { get; private set; }
+      public string Email { get; private set; } 
+    }
+    
+}
