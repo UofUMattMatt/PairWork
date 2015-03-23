8106c747753b1703f6c5d9770fa0e0a89a115195
mahowa <jdbball1@gmail.com>
2015-03-23 10:45:21 -0600

    Connect Class

---
+++ GitHubBrowser/ClassLibrary1/connect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Connect
+    {
+        // You'll need to put your own OAuth token here
+        // It needs to have repo deletion capability
+        private const string TOKEN = "";
+
+        // You'll need to put your own GitHub user name here
+        private const string USER_NAME = "wildenms";
+
+        // You'll need to put your own login name here
+        private const string EMAIL = "wildenms@gmail.com";
+
+        // You'll need to put one of your public REPOs here
+        private const string PUBLIC_REPO = "";
+
+    }
+}
